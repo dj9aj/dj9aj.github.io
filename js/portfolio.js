88bd3b3d845ff652dfcb2015729710e5cf6f2b33
Jack Doyle <jackdo@hotmail.co.uk>
2018-07-22 15:16:31 +0100

    Add header and skill card animations

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,9 +6,20 @@
 //     }
 //   })
 
+const scrollPosY = window.pageYOffset | document.body.scrollTop;
 const body = document.querySelector("#body");
 const navBar = document.querySelector('.navbar');
-const scrollPosY = window.pageYOffset | document.body.scrollTop;
+const aboutHeading = document.querySelector('.about-heading');
+const aboutHr = document.querySelector('.about-hr');
+const skillsHeading = document.querySelector('.skills-heading');
+const skillsHr = document.querySelector('.skills-hr');
+const skill = document.querySelectorAll('.skill');
+const workHeading = document.querySelector('.work-heading');
+const workHr = document.querySelector('.work-hr');
+const contactHeading = document.querySelector('.contact-heading');
+const contactHr = document.querySelector('.contact-hr');
+
+const skills = Array.from(document.querySelectorAll('.skill'));
 
 // body.onscoll = function changeClass() {
 //     nav.classList.add('.active');
@@ -16,16 +27,49 @@ const scrollPosY = window.pageYOffset | document.body.scrollTop;
 
 window.onscroll = function changeNav(){
     const scrollPosY = window.pageYOffset | document.body.scrollTop;
+    // alert(window.scrollY);
 
     if(scrollPosY > 800) {
         navBar.classList.add('active');
+        skillsHeading.classList.add('animateFromLeft');
+        skillsHr.classList.add('animateFromRight');  
+         
+    }
+    
+    if(scrollPosY > 1000) {
+        skills.forEach(skill => {
+            skill.classList.add('flipRound'); 
+        });
+    }
 
-    } else if(scrollPosY <= 400) {
+    if (scrollPosY > 1600) {
+        workHeading.classList.add('animateFromLeft');
+        workHr.classList.add('animateFromRight'); 
+    }    
+
+    
+    if (scrollPosY > 190) {
+        aboutHeading.classList.add('animateFromLeft');
+        aboutHr.classList.add('animateFromRight'); 
+    }  
+    if (scrollPosY > 5200) {
+        contactHeading.classList.add('animateFromLeft');
+        contactHr.classList.add('animateFromRight'); 
+    }  
+
+    if(scrollPosY <= 400) {
         navBar.classList.remove('active'); 
     } 
+    
+    // else if ()
 }
+
 
 
 // var aboutSection = document.querySelector('.section-about');
 
 
+// window.addEventListener('scroll', function(evt) {
+//     aboutTitle.classList.add('animateFromLeft');
+//     aboutHr.classList.add('animateFromRight');
+// });
