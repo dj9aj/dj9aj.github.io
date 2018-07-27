76e061f1e35727337317b9239ffd073782e12754
Jack Doyle <jackdo@hotmail.co.uk>
2018-07-27 21:11:04 +0100

    Add touch event to JS

--- js/portfolio.js
+++ js/portfolio.js
@@ -14,49 +14,36 @@ const elements = {
 }
 
 
-window.addEventListener('scroll', event => {
-    changeNav();
-    removeNav();  
-});
+const animateElements = () => {
 
-
-const changeNav = () => {
-    
     const scrollPosY = window.pageYOffset | document.body.scrollTop;
-    const skills = Array.from(elements.skill);
-
-    // Hide phone nav when user scrolls
-    elements.navBar.id = 'navbar-phone-hidden';  
-
+    
     // Start About section animation
     if (scrollPosY > 190) {
         elements.aboutHeading.classList.add('animateFromLeft');
         elements.aboutHr.classList.add('animateFromRight'); 
     } 
-    // Remove fixed navbar
-    if(scrollPosY <= 400) {
-        elements.navBar.classList.remove('fixed'); 
-    } 
-    // Add fixed navbar
-    if(scrollPosY > 500) {
-        elements.navBar.classList.add('fixed');
-    }     
+
     // Start Skills section animation
     if(scrollPosY > 800) {
         elements.skillsHeading.classList.add('animateFromLeft');
         elements.skillsHr.classList.add('animateFromRight');  
     }
+    
     // Start skills flip animation
+    const skills = Array.from(elements.skill);
     if(scrollPosY > 1000) {
         skills.forEach(skill => {
             skill.classList.add('flipRound'); 
         });
     }
+
     // Start Work section animation
     if (scrollPosY > 1600) {
         elements.workHeading.classList.add('animateFromLeft');
         elements.workHr.classList.add('animateFromRight'); 
     }    
+    
     // Start Contact section animation
     if (scrollPosY > 5200) {
         elements.contactHeading.classList.add('animateFromLeft');
@@ -65,24 +52,28 @@ const changeNav = () => {
 }
 
 
-const removeNav = () => {
-    // Remove navbar dropdown if hamburger is not checked
-    Array.from(elements.links).forEach( el => {
-        el.addEventListener('click', e => {
-            elements.toggle.checked = false;
-        })
-    })
+const changeNav = () => {
+    
+    const scrollPosY = window.pageYOffset | document.body.scrollTop;
+    
+    // Hide phone nav when user scrolls
+    elements.navBar.id = 'navbar-phone-hidden';  
+    
+    // Remove fixed navbar
+    if(scrollPosY <= 400) {
+        elements.navBar.classList.remove('fixed'); 
+    }
 
-    // Detect all clicks on the document
-    window.addEventListener("click touch", function(event) {
-        // If user clicks inside the nav, do nothing
-        if (event.target.closest('.navbar')) return;
+    // Add fixed navbar
+    if(scrollPosY > 500) {
+        elements.navBar.classList.add('fixed');
+    }      
+}
 
-        // If user clicks outside the nav, hide dropdown
-        elements.toggle.checked = false;
-    });
 
-    let isScrolling
+const hideNavOnScroll = () => {
+    
+    let isScrolling;
     // Clear our timeout throughout the scroll
     window.clearTimeout(isScrolling);
 
@@ -92,3 +83,39 @@ const removeNav = () => {
         elements.navBar.id = 'navbar-phone-fixed';
     }, 66); 
 }
+
+
+const checkNavLinks = () => {
+    // Remove navbar dropdown if hamburger is not checked
+    Array.from(elements.links).forEach( el => {
+        el.addEventListener('click', e => {
+            elements.toggle.checked = false;
+        });
+    });
+}
+
+checkNavLinks();
+
+
+// Remove navbar when user clicks away
+const hideNavOnClick = event => {
+    // If user clicks inside nav, return
+    if (event.target.closest('.navbar')) return;
+
+    // If user clicks outside the nav, hide dropdown
+    elements.toggle.checked = false; 
+}
+
+
+// Listen for scroll events
+window.addEventListener('scroll', event => {
+    changeNav();
+    animateElements();
+    hideNavOnScroll();  
+});
+
+
+// Listen for click or touch events
+['click','ontouchstart'].forEach( evt => 
+    window.addEventListener(evt, hideNavOnClick, false)
+);
